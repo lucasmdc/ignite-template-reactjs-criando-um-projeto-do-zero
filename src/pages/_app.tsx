6467cd834e53da0b17ada6e7e2cdf5ab694ff36b
lucasmdc <lucasmdclimao@gmail.com>
2022-08-22 14:05:20 -0300

    add Head component and viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import Header from '../components/Header';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
