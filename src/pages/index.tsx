5a682571d30ca3ceafcdd1979374f22a01197269
lucasmdc <lucasmdclimao@gmail.com>
2022-08-22 09:49:52 -0300

    implemented home (static - structure and styles)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +26,39 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <a>
+          <dl className={styles.box}>
+            <dt className={styles.title}>Como utilizar Hooks</dt>
+            <dd>
+              <p className={styles.subtitle}>
+                Pensando em sincronização em vez de ciclos de vida.
+              </p>
+              <div className={styles.details}>
+                <span className={styles.detail}>
+                  <FiCalendar size={20} />
+                  <time className={styles.detailText} dateTime="2021-03-15">
+                    15 Mar 2021
+                  </time>
+                </span>
+                <span className={styles.detail}>
+                  <FiUser size={20} />
+                  <span className={styles.detailText}>Joseph Oliveira</span>
+                </span>
+              </div>
+            </dd>
+          </dl>
+        </a>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
