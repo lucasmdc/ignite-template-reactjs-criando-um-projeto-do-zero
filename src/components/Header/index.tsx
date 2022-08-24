d6d36b923a45949d01f73714fe38e10939af0dac
lucasmdc <lucasmdclimao@gmail.com>
2022-08-24 14:07:29 -0300

    create link to navigate for home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <img src="/images/Logo.svg" alt="logo" />
+        <Link href="/">
+          <a>
+            <img src="/images/Logo.svg" alt="logo" />
+          </a>
+        </Link>
       </div>
     </header>
   );
