5858a4c709209072073d620d4b4c3a7bcf6fd956
lucasmdc <lucasmdclimao@gmail.com>
2022-08-24 16:15:20 -0300

    implements pagination when user clicked in button for load more posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import { FiCalendar, FiUser } from 'react-icons/fi';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { useState } from 'react';
+import { PrismicDocument, Query } from '@prismicio/types';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -34,13 +36,34 @@ const formatDate = (date: string): string =>
   format(new Date(date), 'd MMM yyyy', { locale: ptBR });
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState<string>(postsPagination.next_page);
+  async function handleClick(): Promise<void> {
+    fetch(nextPage)
+      .then(res => res.json() as Promise<Query>)
+      .then(data => {
+        setPosts(currentPosts => [
+          ...currentPosts,
+          ...data.results.map(post => ({
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author,
+            },
+          })),
+        ]);
+        setNextPage(data.next_page);
+      });
+  }
   return (
     <>
       <Head>
         <title>Home | spacetraveling.</title>
       </Head>
       <main className={styles.contentContainer}>
-        {postsPagination.results.map(post => (
+        {posts.map(post => (
           <Link key={post.uid} href={`/post/${post.uid}`}>
             <a>
               <dl className={styles.box}>
@@ -66,7 +89,11 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
             </a>
           </Link>
         ))}
-        <button type="button">Carregar mais posts</button>
+        {nextPage && (
+          <button type="button" onClick={handleClick}>
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
@@ -74,7 +101,13 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
-  const postsResponse = await prismic.getByType('posts');
+  const postsResponse = await prismic.getByType('posts', {
+    orderings: {
+      field: 'document.first_publication_date',
+      direction: 'desc',
+    },
+    pageSize: 2,
+  });
 
   const { next_page } = postsResponse;
   const results = postsResponse.results.map(post => ({
@@ -86,7 +119,7 @@ export const getStaticProps: GetStaticProps = async () => {
       author: post.data.author,
     },
   }));
-
+  console.log('next_page:', next_page);
   return {
     props: {
       postsPagination: {
